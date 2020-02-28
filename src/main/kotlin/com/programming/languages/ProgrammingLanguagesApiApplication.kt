95be82febd0497b3339d443c3f7d5cdb0491477d
david <david>
2020-02-28 20:36:59 +0100

    initial commit

---
+++ src/main/kotlin/com/programming/languages/ProgrammingLanguagesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.programming.languages
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProgrammingLanguagesApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProgrammingLanguagesApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/programming/languages/ProgrammingLanguagesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.programming.languages
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProgrammingLanguagesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
