d21455a6253602ebf7fc5b7fdd696ac3456d5e8b
david <david>
2020-03-08 10:18:52 +0100

    change to more logic exception

--- src/main/kotlin/com/programming/languages/usecase/exception/NotFoundException.kt
+++ src/main/kotlin/com/programming/languages/usecase/exception/NotFoundException.kt
@@ -3,5 +3,5 @@ package com.programming.languages.usecase.exception
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class NotFoundException(message: String) : RuntimeException(message)
