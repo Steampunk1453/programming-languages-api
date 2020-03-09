f34d6a665c2ff6f399e2e15a31f7bc0b7a62d79d
david <david>
2020-03-09 17:56:08 +0100

    add new endpoints

--- src/main/kotlin/com/programming/languages/rest/LanguageController.kt
+++ src/main/kotlin/com/programming/languages/rest/LanguageController.kt
@@ -1,7 +1,9 @@
 package com.programming.languages.rest
 
 import com.programming.languages.usecase.CreateLanguage
+import com.programming.languages.usecase.RemoveLanguage
 import com.programming.languages.usecase.GetLanguage
+import com.programming.languages.usecase.UpdateLanguage
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -9,17 +11,37 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class LanguageController(
         private val createLanguage: CreateLanguage,
-        private val getLanguage: GetLanguage
+        private val getLanguage: GetLanguage,
+        private val updateLanguage: UpdateLanguage,
+        private val removeLanguage: RemoveLanguage
 ) {
+
     @PostMapping("/language")
     fun createLanguage(@RequestBody languageDto: LanguageDto): ResponseEntity<Unit> {
         createLanguage(languageDto.toDomain())
-        return ResponseEntity(HttpStatus.CREATED)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
     @GetMapping("/language/{languageId}")
     fun getLanguage(@PathVariable languageId: Long): LanguageDto {
         return getLanguage.getById(languageId).toDto()
+    }
+
+    @GetMapping("/language")
+    fun getLanguages(): List<LanguageDto> {
+        return getLanguage.getAll().map { it.toDto() }
+    }
+
+    @PutMapping("/language{languageId}")
+    fun updateLanguage(@RequestBody languageDto: LanguageDto,  @PathVariable languageId: Long): ResponseEntity<Unit> {
+        updateLanguage(languageDto.toDomain(), languageId)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
+    }
+
+    @DeleteMapping("/language/{languageId}")
+    fun deleteLanguage(@PathVariable languageId: Long): ResponseEntity<Unit> {
+        removeLanguage(languageId)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
 }
