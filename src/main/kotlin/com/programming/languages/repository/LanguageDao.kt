56f36e091577b321bf3512794b0ce517f424586f
david <david>
2020-03-09 17:55:27 +0100

    rename method and add new methods

--- src/main/kotlin/com/programming/languages/repository/LanguageDao.kt
+++ src/main/kotlin/com/programming/languages/repository/LanguageDao.kt
@@ -6,12 +6,20 @@ import org.springframework.stereotype.Component
 @Component
 class LanguageDao(private val languageRepository: LanguageRepository) {
 
-    fun create(language: Language): Language {
+    fun save(language: Language): Language {
         return languageRepository.save(language.toEntity()).toDomain()
     }
 
     fun getById(id: Long): Language? {
         return languageRepository.findById(id).orElse(null)?.toDomain()
+    }
+
+    fun getAll(): List<Language>? {
+       return languageRepository.findAll().map { it.toDomain() }
+    }
+
+    fun delete(id: Long) {
+        languageRepository.deleteById(id)
     }
 
 }
