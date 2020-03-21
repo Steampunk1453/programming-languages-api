dd7d84b346aa6835bd40ab186af076545e1c2f39
david <david>
2020-03-21 19:23:40 +0100

    add specific method to find language

--- src/main/kotlin/com/programming/languages/repository/LanguageRepository.kt
+++ src/main/kotlin/com/programming/languages/repository/LanguageRepository.kt
@@ -1,5 +1,9 @@
 package com.programming.languages.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface LanguageRepository : JpaRepository<LanguageEntity, Long>
+interface LanguageRepository : JpaRepository<LanguageEntity, Long> {
+    fun findByName(name: String): Optional<LanguageEntity>
+}
+
