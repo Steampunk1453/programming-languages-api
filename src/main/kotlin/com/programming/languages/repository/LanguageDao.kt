fa77a57a5ab5c37703931fb32bc49b6e4c358f36
david <david>
2020-03-21 19:22:58 +0100

    add methods to get language data

--- src/main/kotlin/com/programming/languages/repository/LanguageDao.kt
+++ src/main/kotlin/com/programming/languages/repository/LanguageDao.kt
@@ -1,10 +1,18 @@
 package com.programming.languages.repository
 
 import com.programming.languages.domain.Language
+import com.programming.languages.repository.http.ApiLanguageRepository
+import com.programming.languages.repository.http.LanguageGithub
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class LanguageDao(private val languageRepository: LanguageRepository) {
+class LanguageDao(private val languageRepository: LanguageRepository,
+                  private val githubLanguageRepository: ApiLanguageRepository
+) {
+
+    @Value("\${github.query}")
+    private lateinit var githubQuery: String
 
     fun save(language: Language): Language {
         return languageRepository.save(language.toEntity()).toDomain()
@@ -20,6 +28,15 @@ class LanguageDao(private val languageRepository: LanguageRepository) {
 
     fun delete(id: Long) {
         languageRepository.deleteById(id)
+    }
+
+    fun getByName(name: String): Language? {
+        return languageRepository.findByName(name).orElse(null)?.toDomain()
+    }
+
+    fun getByNameFromGithub(name: String): LanguageGithub? {
+        val query = githubQuery + name
+        return githubLanguageRepository.getLanguageData(query)
     }
 
 }
