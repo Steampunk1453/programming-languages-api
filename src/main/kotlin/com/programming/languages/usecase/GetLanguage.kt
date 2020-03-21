c6a6169e2911f3c2512a5d2f37dc5a82cd757248
david <david>
2020-03-21 19:27:12 +0100

    get language data from db and github API

--- src/main/kotlin/com/programming/languages/usecase/GetLanguage.kt
+++ src/main/kotlin/com/programming/languages/usecase/GetLanguage.kt
@@ -2,6 +2,10 @@ package com.programming.languages.usecase
 
 import com.programming.languages.domain.Language
 import com.programming.languages.repository.LanguageDao
+import com.programming.languages.repository.http.LanguageGithub
+import com.programming.languages.repository.http.LanguageHttpEntity
+import com.programming.languages.repository.http.toDomain
+import com.programming.languages.repository.http.toEntity
 import com.programming.languages.usecase.exception.NotFoundException
 import org.springframework.stereotype.Service
 
@@ -14,6 +18,13 @@ class GetLanguage(private val languageDao: LanguageDao) {
 
     fun getAll(): List<Language> {
         return languageDao.getAll()?: throw NotFoundException("No languages")
+    }
+
+    fun getByName(name: String): Language {
+        val language = languageDao.getByName(name)?: throw NotFoundException("No language in DB for name: $name")
+        val languageGithub = languageDao.getByNameFromGithub(name)?: throw NotFoundException("No language in Github for name: $name")
+        val languageHttpEntity = languageGithub.toEntity()
+        return languageHttpEntity.toDomain(language)
     }
 
 }
