b1352d8564132b4624c0deea67ab8bdb20e2b49e
david <david>
2020-03-21 19:27:54 +0100

    add to mock restemplate response

--- src/test/kotlin/com/programming/languages/given/GivenLanguage.kt
+++ src/test/kotlin/com/programming/languages/given/GivenLanguage.kt
@@ -1,6 +1,8 @@
 package com.programming.languages.given
 
 import com.programming.languages.domain.Language
+import com.programming.languages.repository.http.Item
+import com.programming.languages.repository.http.LanguageGithub
 
 
 interface GivenLanguage {
@@ -11,8 +13,31 @@ interface GivenLanguage {
                 designed = "JetBrains",
                 year = 2013,
                 version = "1.3.61",
-                web = "https://kotlinlang.org"
+                web = "https://kotlinlang.org",
+                total = null,
+                stars = null,
+                forks = null,
+                watchers = null,
+                openIssues = null
         )
     val LANGUAGE_ID
-     get() = 2
+        get() = 2
+
+    val LANGUAGE_DATA
+        get() = LanguageGithub(
+                total = 1,
+                items = listOf(ITEM)
+
+        )
+
+    val ITEM
+        get() = Item(
+                stars = 2,
+                forks = 3,
+                watchers = 4,
+                openIssues = 5
+        )
+
+    val LANGUAGE_NAME
+        get() = "Spring"
 }
