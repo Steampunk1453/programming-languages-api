b246173d2a9e2aab5d0af0ff2f7572f5944487f7
david <david>
2020-03-22 16:59:30 +0100

    throw exception when language exists

--- src/main/kotlin/com/programming/languages/usecase/CreateLanguage.kt
+++ src/main/kotlin/com/programming/languages/usecase/CreateLanguage.kt
@@ -2,13 +2,18 @@ package com.programming.languages.usecase
 
 import com.programming.languages.domain.Language
 import com.programming.languages.repository.LanguageDao
+import com.programming.languages.usecase.exception.AlreadyRegisteredException
 import org.springframework.stereotype.Service
 
 @Service
 class CreateLanguage(private val languageDao: LanguageDao) {
 
-    operator fun invoke(language: Language): Language {
-       return languageDao.save(language)
+    operator fun invoke(newLanguage: Language): Language? {
+        val language = languageDao.getByName(newLanguage.name)
+        if (language?.name == newLanguage.name) {
+            throw AlreadyRegisteredException("Language already registered: ${newLanguage.name}")
+        }
+        return languageDao.save(newLanguage)
     }
 
 }
