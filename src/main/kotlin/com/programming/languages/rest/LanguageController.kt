41bc02598bcc685ed7649fd7818092cd94366937
david <david>
2020-03-30 00:00:28 +0200

    add parent endpoint and change create response

--- src/main/kotlin/com/programming/languages/rest/LanguageController.kt
+++ src/main/kotlin/com/programming/languages/rest/LanguageController.kt
@@ -1,5 +1,9 @@
 package com.programming.languages.rest
 
+import com.programming.languages.rest.dto.LanguageRequest
+import com.programming.languages.rest.dto.LanguageResponse
+import com.programming.languages.rest.dto.toDomain
+import com.programming.languages.rest.dto.toDto
 import com.programming.languages.usecase.CreateLanguage
 import com.programming.languages.usecase.RemoveLanguage
 import com.programming.languages.usecase.GetLanguage
@@ -9,6 +13,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/language")
 class LanguageController(
         private val createLanguage: CreateLanguage,
         private val getLanguage: GetLanguage,
@@ -16,34 +21,34 @@ class LanguageController(
         private val removeLanguage: RemoveLanguage
 ) {
 
-    @PostMapping("/language")
-    fun createLanguage(@RequestBody languageRequest: LanguageRequest): ResponseEntity<Unit> {
-        createLanguage(languageRequest.toDomain())
-        return ResponseEntity(HttpStatus.NO_CONTENT)
+    @PostMapping
+    fun createLanguage(@RequestBody languageRequest: LanguageRequest): ResponseEntity<LanguageResponse> {
+        val languageResponse = createLanguage(languageRequest.toDomain()).toDto()
+        return ResponseEntity(languageResponse, HttpStatus.CREATED)
     }
 
-    @GetMapping("/language/id/{languageId}")
+    @GetMapping("/id/{languageId}")
     fun getLanguageById(@PathVariable languageId: Long): LanguageResponse {
         return getLanguage.getById(languageId).toDto()
     }
 
-    @GetMapping("/language/name/{name}")
+    @GetMapping("/name/{name}")
     fun getLanguageByName(@PathVariable name: String): LanguageResponse {
         return getLanguage.getByName(name).toDto()
     }
 
-    @GetMapping("/language")
+    @GetMapping
     fun getLanguages(): List<LanguageResponse> {
         return getLanguage.getAll().map { it.toDto() }
     }
 
-    @PutMapping("/language{languageId}")
+    @PutMapping("/{languageId}")
     fun updateLanguage(@RequestBody languageRequest: LanguageRequest, @PathVariable languageId: Long): ResponseEntity<Unit> {
         updateLanguage(languageRequest.toDomain(), languageId)
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
-    @DeleteMapping("/language/{languageId}")
+    @DeleteMapping("/{languageId}")
     fun deleteLanguage(@PathVariable languageId: Long): ResponseEntity<Unit> {
         removeLanguage(languageId)
         return ResponseEntity(HttpStatus.NO_CONTENT)
