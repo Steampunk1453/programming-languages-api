f25e011b03d939d9aedd38a0d42f752fdd862607
david <david>
2020-03-29 23:52:30 +0200

    use cases to login a user and exceptions

---
+++ src/main/kotlin/com/programming/languages/usecase/CreateUser.kt
@@ -0,0 +1,21 @@
+package com.programming.languages.usecase
+
+import com.programming.languages.domain.User
+import com.programming.languages.repository.UserDao
+import com.programming.languages.usecase.exception.EmailAlreadyUsedException
+import com.programming.languages.usecase.exception.LoginAlreadyUsedException
+import org.springframework.stereotype.Component
+
+@Component
+class CreateUser(private val userDao: UserDao) {
+
+    operator fun invoke(user: User): User {
+        if (userDao.getByUsername(user.username.toLowerCase()).isPresent) {
+            throw LoginAlreadyUsedException("User ${user.username} already in use")
+        } else if (userDao.getByEmail(user.email.toLowerCase()).isPresent) {
+            throw EmailAlreadyUsedException("User ${user.email} already in use")
+        }
+        return userDao.create(user)
+    }
+
+}
---
+++ src/main/kotlin/com/programming/languages/usecase/exception/LoginAlreadyUsedException.kt
@@ -0,0 +1,7 @@
+package com.programming.languages.usecase.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.CONFLICT)
+class LoginAlreadyUsedException(message: String) : RuntimeException(message)
