116bd616dfe9754233c0487c79d12a338ee0cedb
david <david>
2020-03-29 23:54:59 +0200

    remove interface It's not necessary

--- src/main/kotlin/com/programming/languages/repository/http/GithubLanguageRepository.kt
+++ src/main/kotlin/com/programming/languages/repository/http/GithubLanguageRepository.kt
@@ -1,21 +1,20 @@
 package com.programming.languages.repository.http
 
+import com.programming.languages.repository.http.dto.LanguageGithub
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Repository
 import org.springframework.web.client.RestTemplate
 
 @Repository
-class GithubLanguageRepository(
-        private val restTemplate: RestTemplate
-) : ApiLanguageRepository {
+class GithubLanguageRepository(private val restTemplate: RestTemplate)  {
 
     @Value("\${github.url}")
     private lateinit var githubUrl: String
 
     private val logger = KotlinLogging.logger {}
 
-    override fun getLanguageData(query: String): LanguageGithub? {
+    fun getLanguageData(query: String): LanguageGithub? {
         logger.debug { "URL $githubUrl$query" }
         return restTemplate.getForEntity(
                 githubUrl + query,
