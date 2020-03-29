130e001953a83da0625a63052a37df5460da7593
david <david>
2020-03-29 23:58:34 +0200

    move interface to dao

--- src/main/kotlin/com/programming/languages/repository/LanguageRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.programming.languages.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface LanguageRepository : JpaRepository<LanguageEntity, Long> {
-    fun findByName(name: String?): Optional<LanguageEntity>
-}
-
